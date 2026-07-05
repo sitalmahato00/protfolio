b296e643324767f7ccf2280682f66848bdbd5b9b
sital <sitalmahato077@gmail.com>
2026-07-05 11:03:30 +0545

    fix: escape blade vite interpolation causing parse error

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -196,7 +196,7 @@
         <!-- Scripts -->
         @routes
         @viteReactRefresh
-        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
+        @vite(['resources/js/app.jsx', "resources/js/Pages/".($page['component']).".jsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
